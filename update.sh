3f4a9cdd0bb30193b761be84b84a628e831448d1
Anfernee Gui <agui@vmware.com>
2013-03-22 17:22:55 +0800

    Add update script

--- update.sh
+++ update.sh
@@ -1,4 +1,44 @@
-#!/bin/sh
+#!/bin/bash
+
+function save()
+{
+	local file back_file
+	file=$1
+
+	if [ -e $file ]; then
+		back_file=$file.$(date +%s).back
+
+		echo Backing up $file to $back_file
+		mv $file $back_file
+	fi
+}
+
+function update()
+{
+	local file newfile
+	file=$1
+
+	echo Updating $file
+
+	if [[ "$file" =~ ^_ ]]; then
+		# Why this doesn't work?
+		# newfile=${file//^_/.}
+
+		newfile=$(echo $file | sed 's/^_/./g')
+		newfile=~/$newfile
+	fi
+
+	save $newfile
+
+	ln -s $(readlink -f $file) $newfile
+}
 
 git submodule update --init
 
+pushd files
+
+for file in `ls`; do
+	update $file
+done
+
+popd
